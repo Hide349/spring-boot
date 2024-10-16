6591f956fb717b7aca3e26c5051052649aa41f8d
Ruan Pablo <ruanpablooliveira20@gmail.com>
2024-10-16 17:12:00 -0300

    Order, Instant

--- curso/src/main/java/com/aprendendospring/curso/config/TestConfig.java
+++ curso/src/main/java/com/aprendendospring/curso/config/TestConfig.java
@@ -1,5 +1,6 @@
 package com.aprendendospring.curso.config;
 
+import java.time.Instant;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +8,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.aprendendospring.curso.entities.Order;
 import com.aprendendospring.curso.entities.User;
+import com.aprendendospring.curso.repositories.OrderRepository;
 import com.aprendendospring.curso.repositories.UserRepository;
 
 @Configuration
@@ -16,6 +19,9 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private OrderRepository orderRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -23,7 +29,13 @@ public class TestConfig implements CommandLineRunner{
 		User u1 = new User(null,"Maria Brown","maria@gmail.com","988888","123456");
 		User u2 = new User(null,"Alex Green","alex@gmail.com","9777777","123456");
 		
+		Order o1 = new Order(null,Instant.parse("2019-06-20T19:53:07Z"),u1);
+		Order o2 = new Order(null,Instant.parse("2019-07-21T19:53:07Z"),u2);
+		Order o3 = new Order(null,Instant.parse("2019-07-22T19:53:07Z"),u1);
+
+		
 		userRepository.saveAll(Arrays.asList(u1,u2));
+		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
 	}
 
 	
--- curso/src/main/java/com/aprendendospring/curso/services/UserService.java
+++ curso/src/main/java/com/aprendendospring/curso/services/UserService.java
@@ -23,7 +23,6 @@ public class UserService {
 	public User findById(Long id) {
 		Optional<User> obj =  repository.findById(id);
 		return obj.get();
-	
 	}
 	
 	
