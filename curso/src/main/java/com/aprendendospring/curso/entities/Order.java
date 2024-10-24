f5039002f6596c4d7ba5374415eac854db34d87a
Ruan Pablo <ruanpablooliveira20@gmail.com>
2024-10-24 09:57:22 -0300

    Entitie Payment, association OneToOne with order

--- curso/src/main/java/com/aprendendospring/curso/entities/Order.java
+++ curso/src/main/java/com/aprendendospring/curso/entities/Order.java
@@ -9,6 +9,7 @@ import java.util.Set;
 import com.aprendendospring.curso.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -16,6 +17,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 
@@ -42,6 +44,9 @@ public class Order implements Serializable{
 	
 	@OneToMany(mappedBy = "id.order")
 	private Set<OrderItem> items = new HashSet<>();
+	
+	@OneToOne(mappedBy = "order",cascade = CascadeType.ALL)
+	private Payment payment;
 	
 	public Order() {
 		
@@ -76,6 +81,18 @@ public class Order implements Serializable{
 	public Instant getMoment() {
 		return moment;
 	}
+	
+	
+
+	public Payment getPayment() {
+		return payment;
+	}
+
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
+	}
+
 
 	public void setMoment(Instant moment) {
 		this.moment = moment;
